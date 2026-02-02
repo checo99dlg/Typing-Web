eb67189ed147a6e89df7e88012649af3544f54b1
Sergio De La Garza <sergiodelagarza@Checos-MacBook-Pro.local>
2026-02-02 13:11:04 -0600

    Merge alembic heads

---
+++ migrations/versions/3b4a1d7c9e2f_merge_heads.py
@@ -0,0 +1,21 @@
+"""merge heads
+
+Revision ID: 3b4a1d7c9e2f
+Revises: 2c8b5f92d8a1, 8f2b3c9a1f0e
+Create Date: 2026-02-02 00:00:00.000000
+"""
+
+from alembic import op
+
+revision = "3b4a1d7c9e2f"
+down_revision = ("2c8b5f92d8a1", "8f2b3c9a1f0e")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
